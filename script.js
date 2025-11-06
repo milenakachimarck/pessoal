ab65c37a4821a6bd5507828bc42f23b73716ba26
milenak <milena.kachimarck@escola.pr.gov.br>
2025-11-06 19:51:07 -0300

    .

--- script.js
+++
@@ -1,8 +0,0 @@
-// Exemplo: mostra uma mensagem quando clicar em um botão
-const botoes = document.querySelectorAll(".botoes button");
-
-botoes.forEach(btn => {
-  btn.addEventListener("click", () => {
-    alert(`Você clicou em "${btn.textContent}"`);
-  });
-});
