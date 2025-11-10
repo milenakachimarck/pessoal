f2040d7ca9a315faf3aa921ae6782a3f945d6fee
milenak <milena.kachimarck@escola.pr.gov.br>
2025-11-10 11:26:51 -0300

    fv

--- script.js
+++
@@ -1,8 +0,0 @@
-// Exemplo: rolar suavemente para seções
-document.querySelectorAll('.menu a').forEach(link => {
-  link.addEventListener('click', e => {
-    e.preventDefault();
-    const id = e.target.getAttribute('href');
-    document.querySelector(id)?.scrollIntoView({ behavior: 'smooth' });
-  });
-});
